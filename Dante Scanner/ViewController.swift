e43d79c765c7086691dc3edbb1a340e2a3b430c5
Hung Phan <hpphann@gmail.com>
2019-08-13 12:36:43 -0700

    first commit

---
+++ Dante Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dante Scanner
+//
+//  Created by Hung Phan on 8/13/19.
+//  Copyright © 2019 Hung Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
