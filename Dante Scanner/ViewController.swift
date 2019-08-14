9da565cd47262df1a89d03a1b127c5d72450173a
Hung Phan <hpphann@gmail.com>
2019-08-14 16:41:42 -0700

    added app-icon, UI

--- Dante Scanner/ViewController.swift
+++ Dante Scanner/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passcodeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        passcodeTextField.layer.cornerRadius = 10
+        
     }
 
 
